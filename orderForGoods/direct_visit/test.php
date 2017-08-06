55ace6f4a7cec25c26c3bb58ed116a29b8f545c3
liuqiuchen <2016968116@qq.com>
2017-08-06 09:02:16 +0800

    commit

--- orderForGoods/direct_visit/test.php
+++ orderForGoods/direct_visit/test.php
@@ -1,2 +1,14 @@
 <?php
-echo substr_replace('Hello world', 'earth', 6); //Hello earth
+function create_table($data) {
+    echo "<table border='1'>";
+    reset($data);
+    $value = current($data);
+    while($value) {
+        echo "<tr><td>$value</td></tr>\n";
+        $value = next($data);
+    }
+    echo '<table/>';
+}
+
+$my_array = array('Line one', 'Line two', 'Line three');
+create_table($my_array);
