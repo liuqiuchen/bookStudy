d0f7bf9af9b743330991e10b658d97bd7f2607fb
liuqiuchen <2016968116@qq.com>
2017-09-26 22:43:24 +0800

    commit

--- orderForGoods/direct_visit/home.php
+++ orderForGoods/direct_visit/home.php
@@ -1,38 +1,10 @@
 <?php
-require('../common/header.php');
-?>
+require('../common/includes/Page.inc');
 
-<!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
-    <div class="container">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="http://v3.bootcss.com/examples/jumbotron/#" role="button">Learn more »</a></p>
-    </div>
-</div>
+$home_page = new Page();
+$home_page->content = "<p>Welcome to the home of TLA Consulting.
+                        Please take some time to get to know us.</p>
+                        <p>We specialize in serving your business needs and hope
+                        to hear from you soon.</p>";
+$home_page->Display();
 
-<div class="container">
-    <!-- Example row of columns -->
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="http://v3.bootcss.com/examples/jumbotron/#" role="button">View details »</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="http://v3.bootcss.com/examples/jumbotron/#" role="button">View details »</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="http://v3.bootcss.com/examples/jumbotron/#" role="button">View details »</a></p>
-        </div>
-    </div>
-
-    <hr>
-
-<?php
-require('../common/footer.php');
-?>
--- orderForGoods/direct_visit/test.php
+++ orderForGoods/direct_visit/test.php
@@ -25,9 +25,9 @@ class Page
         $this->DisplayStyles();
         echo "</head>\n<body>\n";
         $this->DisplayHeader();
-        //$this->DisplayMenu($this->buttons);
+        $this->DisplayMenu($this->buttons);
         echo $this->content;
-        //$this->DisplayFooter();
+        $this->DisplayFooter();
         echo "</body>\n</html>\n";
     }
     public function DisplayTitle()
@@ -90,6 +90,53 @@ class Page
     </table>
 <?php
     }
+    public function DisplayMenu($buttons)
+    {
+        echo "<table width='100%' bgcolor='white' cellpadding='4' cellspacing='4'>\n";
+        echo "<tr>\n";
+        $width = 100/count($buttons);
+        while(list($name, $url) = each($buttons)) {
+            $this->DisplayButton($width, $name, $url, !$this->IsURLCurrentPage($url));
+        }
+        echo "</tr>\n";
+        echo "</table>\n";
+    }
+
+    public function IsURLCurrentPage($url) {
+        // $_SERVER['PHP_SELF'] 引用当前网页地址
+        //echo $_SERVER['PHP_SELF']."<br/>";
+        if(strpos($_SERVER['PHP_SELF'], $url) == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function DisplayButton($width, $name, $url, $active = true) {
+        if($active) {
+            echo "<td width='".$width."%'>
+            <a href='".$url."'><span class='menu'>".$name."</span></a>
+            </td>";
+        } else {
+            echo "<td width='".$width."%'>
+            <a href='".$url."'><span class='menu'>".$name."</span></a>
+            </td>";
+        }
+    }
+
+    public function DisplayFooter() {
+?>
+        <table width="100%" bgcolor="black" cellpadding="12" border="0">
+            <tr>
+                <td>
+                    <p class="foot">&copy; TLA Consulting Pty Ltd.</p>
+                    <p class="foot">Please see our <a href="">legal information page</a></p>
+                </td>
+            </tr>
+        </table>
+<?php
+    }
+
 }
 
 $page = new Page();
